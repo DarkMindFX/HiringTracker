e6220744fe045e513b453ff21d238e36271279e7
DarkMindFX <contact@darkmindfx.com>
2021-05-10 23:53:18 +0200

    Adding controls to candidate form

--- Sources/hrt.tracker-ui/src/pages/candidate.jsx
+++ Sources/hrt.tracker-ui/src/pages/candidate.jsx
@@ -32,6 +32,16 @@ class CandidatePage extends React.Component {
             error: null,
             success: null
         }
+
+        this.onSkillAdded = this.onSkillAdded.bind(this);
+        this.onSkillChanged = this.onSkillChanged.bind(this);
+        this.onSkillDeleted = this.onSkillDeleted.bind(this);
+        this.onFirstNameChanged = this.onFirstNameChanged.bind(this);
+        this.onMiddleNameChanged = this.onMiddleNameChanged.bind(this);
+        this.onLastNameChanged = this.onLastNameChanged.bind(this);
+        this.onEmailChanged = this.onEmailChanged.bind(this);
+        this.onPhoneChanged = this.onPhoneChanged.bind(this);
+        this.onCVLinkChanged = this.onCVLinkChanged.bind(this);
     }
 
     onFirstNameChanged(event) {
@@ -51,6 +61,10 @@ class CandidatePage extends React.Component {
     }
 
     onEmailChanged(event) {
+
+    }
+
+    onCVLinkChanged(event) {
 
     }
 
@@ -95,4 +109,204 @@ class CandidatePage extends React.Component {
             this.setState(updatedState);
         }
     }
+
+    onSaveClicked() {
+
+    }
+
+    onDeleteClicked() {
+
+    }
+
+    onDeleteCancel() {
+
+    }
+
+    onDeleteConfirm() {
+
+    }
+
+    
+
+    render() {
+        let skills = this._getCandidateSkills();
+
+        const styleError = {
+            display: this.state.showError ? "block" : "none"
+        }
+
+        const styleSuccess = {
+            display: this.state.showSuccess ? "block" : "none"
+        }   
+        
+        const styleDeleteBtn = {
+            display: this.state.id ? "block" : "none"
+        }
+
+        return (
+            <div>
+                 <table>
+                    <tbody>
+                        <tr>
+                            <td style={{width: 450}}></td>
+                            <td>
+                                <Button variant="contained" color="primary"
+                                        onClick={ () => this.onSaveClicked() }>Save</Button>
+
+                                <Button variant="contained" color="secondary"
+                                        style={styleDeleteBtn}
+                                        onClick={ () => this.onDeleteClicked() }>Delete</Button>
+
+                                <Button variant="contained" component={Link} to="/candidates">Cancel</Button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <Alert severity="error" style={styleError}>Error: {this.state.error}</Alert>
+                                <Alert severity="success" style={styleSuccess}>Success! {this.state.success}</Alert>
+                                
+                            </td>
+                        </tr>                    
+                        <tr>                            
+                            <td colSpan={2}>
+                                
+                                <TextField  id="firstName" 
+                                            fullWidth
+                                            type="text" 
+                                            variant="filled" 
+                                            label="First Name" 
+                                            value={this.state.position._fname}
+                                            onChange={ (event) => { this.onFirstNameChanged(event) } }
+                                            />
+                                
+                            </td>
+                        </tr>                    
+                        <tr>
+                            <td colSpan={2}>
+                                <TextField  id="middleName" 
+                                            fullWidth
+                                            type="text" 
+                                            variant="filled" 
+                                            label="Middle Name" 
+                                            value={this.state.position._mname}
+                                            onChange={ (event) => { this.onMiddleNameChanged(event) } }
+                                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={1}>
+                                <TextField  id="lastName" 
+                                            fullWidth
+                                            type="text" 
+                                            variant="filled" 
+                                            label="Last Name" 
+                                            value={this.state.position._lname}
+                                            onChange={ (event) => { this.onLastNameChanged(event) } }
+                                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={1}>
+                                <TextField  id="email" 
+                                            fullWidth
+                                            type="text" 
+                                            variant="filled" 
+                                            label="Email" 
+                                            value={this.state.position._email}
+                                            onChange={ (event) => { this.onEmailChanged(event) } }
+                                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={1}>
+                                <TextField  id="phone" 
+                                            fullWidth
+                                            type="text" 
+                                            variant="filled" 
+                                            label="Phone" 
+                                            value={this.state.position._phone}
+                                            onChange={ (event) => { this.onPhoneChanged(event) } }
+                                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={1}>
+                                <TextField  id="cvlink" 
+                                            fullWidth
+                                            type="text" 
+                                            variant="filled" 
+                                            label="Link to CV" 
+                                            value={this.state.position._cvlink}
+                                            onChange={ (event) => { this.onCVLinkChanged(event) } }
+                                            />
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td colSpan={4}>
+                                <SkillsList id="candidateSkills"
+                                    skills={ skills }
+                                    canEdit={ this.state.canEdit }
+                                    onSkillAdded = { this.onSkillAdded }
+                                    onSkillChanged = { this.onSkillChanged }
+                                    onSkillDeleted = { this.onSkillDeleted }
+                                 />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <Dialog open={this.state.showDeleteConfirm} onClose={() => { this.onDeleteCancel() }} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Delete Candidate</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete this candidate?
+                    </DialogContentText>                    
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={() => { this.onDeleteCancel() }} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={() => { this.onDeleteConfirm() }} color="primary">
+                        Delete
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        )
+    }
+
+    _createEmptyCandidateObj() {
+        let cand = {
+            _fname: "",
+            _lname: "",
+            _mname: "",
+            _phone: "",
+            _email: "",
+            _cvlink: "",
+            _skills: []
+        }
+
+        return cand;
+    }
+
+    _getPositionSkills() {
+        let skills = [];
+
+        if( this.state.candidate._skills ) {
+            
+            this.state.candidate._skills.forEach( s => {
+                let skill = {
+                    id: s.id,
+                    SkillID: s._skill._skillId, 
+                    ProficiencyID: s._proficiency._id
+                };
+                skills.push(skill);
+            })
+        }  
+      
+        return skills;
+    }
 }
+
+export default withRouter(CandidatePage);
